d2b291d924879c21686e49fe91a4798469d6f48b
Andreas Herz <a.herz@freegroup.de>
2020-04-24 12:59:46 +0200

    Update brainbox.ino

--- arduino/brainbox.ino
+++ arduino/brainbox.ino
@@ -1,23 +1,23 @@
 /* credits to http://forums.trossenrobotics.com/tutorials/how-to-diy-128/complete-control-of-an-arduino-via-serial-3300/
- * 1 = Wright:
- *     1 = analog:
- *         "pin number"
- *         "1 for LOW"
- *         "2 for HIGH"
- *     2 = digital:
- *         "pin number"
- *         "frequency (0-255)"
- *  2 = Read:
- *     1 = analog:
- *         "pin number"
- *     2 = digital:
- *         "pin number"
- *
- *  '1/2/7/1/' will turn pin 7 on HIGH
- *  '1/2/7/0/' would turn pin 7 to LOW
- *  '1/1/7/255/' would turn pin 7 on at a analog rate of 255 or full power
- *
- */
+   1 = Wright:
+       1 = analog:
+           "pin number"
+           "1 for LOW"
+           "2 for HIGH"
+       2 = digital:
+           "pin number"
+           "frequency (0-255)"
+    2 = Read:
+       1 = analog:
+           "pin number"
+       2 = digital:
+           "pin number"
+
+    '1/2/7/1/' will turn pin 7 on HIGH
+    '1/2/7/0/' would turn pin 7 to LOW
+    '1/1/7/255/' would turn pin 7 on at a analog rate of 255 or full power
+
+*/
 
 // keep in mind to update the line `#define USB_VERSION 0x200` and change `0x200` to `0x210`.
 // in the WebUSB.h file. USB 2.1 is required so that the host knows that the device exposes a
@@ -29,7 +29,7 @@
 // WebUSB WebUSBSerial(0, "localhost:7400/circuit/?tutorial=pairWebUSB");
 
 // for productive ( 1 = HTTPS, 0 = HTTP) HTTP is only allowed for "localhost".
-WebUSB WebUSBSerial(1, "www.brainbox-demo.de/circuit/?global=/guides/PairWebUSB.brain");
+WebUSB WebUSBSerial(1, "www.brainbox-demo.de/circuit/?global=guides/PairWebUSB.brain");
 #define Serial WebUSBSerial
 
 unsigned long serialdata;
@@ -43,85 +43,87 @@ int sensorVal;
 int analogRate;
 int digitalState;
 
-void setup(){
-  while (!Serial) {;}
+void setup() {
+  while (!Serial) {
+    ;
+  }
   Serial.begin(9600);
   Serial.println("Arduino with Brainbox sketch up and running");
 }
 
-void loop(){
+void loop() {
   getSerial();
-  switch(serialdata){
+  switch (serialdata) {
     case 1:
-    {
-      //analog digital write
-      getSerial();
-      switch (serialdata){
-        case 1:
-        {
-          //analog write
-          getSerial();
-          pinNumber = serialdata;
-          getSerial();
-          analogRate = serialdata;
-          pinMode(pinNumber, OUTPUT);
-          analogWrite(pinNumber, analogRate);
-          pinNumber = 0;
-          break;
-        }
-        case 2:
-        {
-          //digital write
-          getSerial();
-          pinNumber = serialdata;
-          getSerial();
-          digitalState = serialdata;
-          pinMode(pinNumber, OUTPUT);
-          if (digitalState == 1){
-            digitalWrite(pinNumber, LOW);
-          }
-          if (digitalState == 2){
-            digitalWrite(pinNumber, HIGH);
-          }
-          pinNumber = 0;
-          break;
-
-        }
-     }
-     break;
-    }
-    case 2:
-    {
-      getSerial();
-      switch (serialdata)
       {
-        case 1:
-        {
-          //analog read
-          getSerial();
-          pinNumber = serialdata;
-          pinMode(pinNumber, INPUT);
-          sensorVal = analogRead(pinNumber);
-          Serial.println(sensorVal);
-          sensorVal = 0;
-          pinNumber = 0;
-          break;
+        //analog digital write
+        getSerial();
+        switch (serialdata) {
+          case 1:
+            {
+              //analog write
+              getSerial();
+              pinNumber = serialdata;
+              getSerial();
+              analogRate = serialdata;
+              pinMode(pinNumber, OUTPUT);
+              analogWrite(pinNumber, analogRate);
+              pinNumber = 0;
+              break;
+            }
+          case 2:
+            {
+              //digital write
+              getSerial();
+              pinNumber = serialdata;
+              getSerial();
+              digitalState = serialdata;
+              pinMode(pinNumber, OUTPUT);
+              if (digitalState == 1) {
+                digitalWrite(pinNumber, LOW);
+              }
+              if (digitalState == 2) {
+                digitalWrite(pinNumber, HIGH);
+              }
+              pinNumber = 0;
+              break;
+
+            }
         }
-        case 2:
-        {
-          //digital read
-          getSerial();
-          pinNumber = serialdata;
-          pinMode(pinNumber, INPUT);
-          sensorVal = digitalRead(pinNumber);
-          Serial.println(sensorVal);
-          sensorVal = 0;
-          pinNumber = 0;
-          break;
-        }
+        break;
       }
-      break;
-    }
+    case 2:
+      {
+        getSerial();
+        switch (serialdata)
+        {
+          case 1:
+            {
+              //analog read
+              getSerial();
+              pinNumber = serialdata;
+              pinMode(pinNumber, INPUT);
+              sensorVal = analogRead(pinNumber);
+              Serial.println(sensorVal);
+              sensorVal = 0;
+              pinNumber = 0;
+              break;
+            }
+          case 2:
+            {
+              //digital read
+              getSerial();
+              pinNumber = serialdata;
+              pinMode(pinNumber, INPUT);
+              sensorVal = digitalRead(pinNumber);
+              Serial.println(sensorVal);
+              sensorVal = 0;
+              pinNumber = 0;
+              break;
+            }
+        }
+        break;
+      }
   }
 }
 
